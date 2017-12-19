ba98166b4bd58906c87e038348c5c4b381ed01cc
Azhaziq Goh <muhammad-azhaziq.bin-mohd-azlan-goh@hpe.com>
2017-12-19 23:59:29 +0800

    add more initial data

--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -5,6 +5,8 @@ export class InMemoryDataService implements InMemoryDbService {
     const ledgerEntries = [
       { id: 1, date: '2017-12-01', description: 'Salary', category: 'Income', debit: 3000, credit: 0},
       { id: 2, date: '2017-12-02', description: 'Lunch', category: 'Food', debit: 0, credit: 15},
+      { id: 3, date: '2017-12-03', description: 'Shopping', category: 'Others', debit: 0, credit: 200},
+      { id: 4, date: '2017-12-04', description: 'Movie', category: 'Entertainment', debit: 0, credit: 20}
     ];
     return {ledgerEntries};
   }
