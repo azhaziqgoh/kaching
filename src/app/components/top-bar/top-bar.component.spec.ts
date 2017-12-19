5ab6382e74a246e9159c9c1fe4e4a55efd2c96c5
Azhaziq Goh <muhammad-azhaziq.bin-mohd-azlan-goh@hpe.com>
2017-12-20 01:09:02 +0800

    init test

--- src/app/components/top-bar/top-bar.component.spec.ts
+++ src/app/components/top-bar/top-bar.component.spec.ts
@@ -1,14 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { TopBarComponent } from './top-bar.component';
 
-// describe('TopBarComponent', () => {
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [
-//         TopBarComponent
-//       ],
-//     }).compileComponents();
-//   }));
+describe('TopBarComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        TopBarComponent
+      ],
+    }).compileComponents();
+  }));
 //   it('should create the app', async(() => {
 //     const fixture = TestBed.createComponent(TopBarComponent);
 //     const app = fixture.debugElement.componentInstance;
@@ -25,4 +25,4 @@ import { TopBarComponent } from './top-bar.component';
 //     const compiled = fixture.debugElement.nativeElement;
 //     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
 //   }));
-//});
+});
